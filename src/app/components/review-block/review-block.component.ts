df247ea0647fe35e06ca3828babb38e202955414
prajesh-projects <70057736+prajesh-projects@users.noreply.github.com>
2024-09-07 22:48:30 +0530

    deploy

--- src/app/components/review-block/review-block.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-review-block',
-  templateUrl: './review-block.component.html',
-  styleUrl: './review-block.component.scss',
-})
-export class ReviewBlockComponent {}
--- src/app/pages/dialogs/pickup-details-dailog/pickup-details-dailog.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-pickup-details-dailog',
-  templateUrl: './pickup-details-dailog.component.html',
-  styleUrl: './pickup-details-dailog.component.scss',
-})
-export class PickupDetailsDailogComponent {}
